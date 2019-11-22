9b6f817c6dc8827e8774ea7e449dc17130137b31
dmlyons2 <dmlyons2@gmail.com>
2019-11-22 10:09:26 -0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Container, Row, Col, Jumbotron } from 'react-bootstrap'
+import { Container, Navbar } from 'react-bootstrap'
 import Results from "./components/Results";
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar fixed="top" bg="light" variant="light">
+      <Navbar.Brand href="#Expodite">
+        Expodite
+      </Navbar.Brand>
+    </Navbar>
       <Container>
         <p />
-        <Jumbotron>
-          <Row>
-            <Col>
-              <h1>expodite-app</h1>
-              <p>
-                This is a simple mockup of Kristen's chemical ranking script linking back to supporting documents.
-              </p>
-            </Col>
-          </Row>
-        </Jumbotron>
+
       </Container>
       <Results/>
 
