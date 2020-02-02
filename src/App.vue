09baa1ea8a4df76ae9006ec89f7d31539008a9e5
dlyons <dmlyons2@gmail.com>
2020-02-02 14:25:37 -0600

    New Vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <div class="row">
+            <div class="col-8">
+                <MyGrid />
+            </div>
+            <div class="col-4">
+                <ChemicalDetail />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MyGrid from "@/components/MyGrid";
+import ChemicalDetail from "@/components/ChemicalDetail";
+
+  export default {
+    name: 'app',
+    components: {
+      MyGrid, ChemicalDetail
+    }
+  }
+</script>
+
+
---
+++ src/components/ChemicalDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Chemical Detail</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChemicalDetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
