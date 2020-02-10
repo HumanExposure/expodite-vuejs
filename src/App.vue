d7cc37d71e19affd85d328ed0667cd0e095f0f08
dlyons <dmlyons2@gmail.com>
2020-02-10 11:38:32 -0600

    Details show

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
     <div id="app">
         <div class="row">
-            <div class="col-8">
-                <ChemicalGrid></ChemicalGrid>
+            <div class="col-6">
+                <h2>{{ title }}</h2>
+                <div>
+                    Sorting By: <b>{{ sortBy }}</b>, Sort Direction:
+                    <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b>
+                </div>
+                <b-table
+                        :items="chemicals.data"
+                        :fields="fields"
+                        :sort-by.sync="sortBy"
+                        :sort-desc.sync="sortDesc"
+                        :select-mode="selectMode"
+                        @row-selected="onRowSelected"
+                        sort-icon-left
+                        responsive="sm"
+                        hover
+                        striped
+                        selectable
+                        small
+                ></b-table>
             </div>
-            <div class="col-4">
-                <ChemicalDetail></ChemicalDetail>
+            <div class="col-6" v-if="selected[0]">
+                <h2>{{ selected.PREFERRED_NAME }}</h2>
+                <ul>
+                    <li v-for="(item) in selected[0].DETAILS" :key="item.id">
+                        <h3>Score {{ item.Score }}</h3>
+                        <ul>
+                            <li>Differential Metric: {{ item.differential_metric }}</li>
+                            <li v-if="item.gen_cat">PUC: {{ item.gen_cat }}</li>
+                            <li v-if="item.puc_id">PUC_ID: {{ item.puc_id }}</li>
+                            <li v-if="item.Name">Name: {{ item.Name }}</li>
+                            <li>Product ids: {{ item.Product_ids }}</li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import ChemicalGrid from "./components/ChemicalGrid"
-import ChemicalDetail from "./components/ChemicalDetail"
+    import axios from 'axios';
 
-
-  export default {
-    name: 'app',
-    components: {
-      ChemicalGrid, ChemicalDetail
-    },
-  }
+    export default {
+        name: 'app',
+        components: {},
+        data() {
+            return {
+                title: "My Chemical List",
+                sortBy: 'SCORE',
+                sortDesc: false,
+                selected: [],
+                modes: ['multi', 'single', 'range'],
+                selectMode: 'single',
+                chemicals: null,
+                chemical: null,
+                details: null,
+                fields: [
+                    {key: 'PREFERRED_NAME', label: 'Chemical', sortable: true},
+                    {key: 'DTXSID', sortable: false},
+                    {key: 'SCORE', label: 'Score', sortable: true}
+                ],
+            }
+        },
+        mounted() {
+            axios
+                .get('http://localhost:3000/chemicals')
+                .then(response => (this.chemicals = response));
+        },
+        methods: {
+            onRowSelected(chemical) {
+                this.selected = chemical;
+            }
+        }
+    }
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
-// Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
-Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app' );
+
