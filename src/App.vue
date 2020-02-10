a1ac315975c5c81ab6645fea70d1b0c8fcd538ee
dlyons <dmlyons2@gmail.com>
2020-02-10 12:58:43 -0600

    Details on Card

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
                     Sorting By: <b>{{ sortBy }}</b>, Sort Direction:
                     <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b>
                 </div>
-                <b-table
-                        :items="chemicals.data"
+                <b-table class="shadow-lg"
+                         style="background-color: white"
+                        :items="chemicals.chemicals"
                         :fields="fields"
                         :sort-by.sync="sortBy"
                         :sort-desc.sync="sortDesc"
@@ -23,18 +24,18 @@
                 ></b-table>
             </div>
             <div class="col-6" v-if="selected[0]">
-                <h2>{{ selected.PREFERRED_NAME }}</h2>
+                <h3>{{ selected[0].PREFERRED_NAME }}</h3>
                 <ul>
-                    <li v-for="(item) in selected[0].DETAILS" :key="item.id">
-                        <h3>Score {{ item.Score }}</h3>
-                        <ul>
-                            <li>Differential Metric: {{ item.differential_metric }}</li>
-                            <li v-if="item.gen_cat">PUC: {{ item.gen_cat }}</li>
-                            <li v-if="item.puc_id">PUC_ID: {{ item.puc_id }}</li>
-                            <li v-if="item.Name">Name: {{ item.Name }}</li>
-                            <li>Product ids: {{ item.Product_ids }}</li>
-                        </ul>
-                    </li>
+                    <div v-for="(item) in selected[0].DETAILS" :key="item.id">
+                        <b-card class="shadow" style="margin-bottom: 15px">
+                        <b-card-title>{{ item.differential_metric}}</b-card-title>
+                            <b-card-sub-title>Score: {{ item.Score}} </b-card-sub-title>
+                            <b-card-text v-if="item.gen_cat">PUC: {{ item.gen_cat }}</b-card-text>
+                            <b-card-text v-if="item.puc_id">PUC_ID: {{ item.puc_id }}</b-card-text>
+                            <b-card-text v-if="item.Name">Name: {{ item.Name }}</b-card-text>
+                            <b-card-text>Product ids: {{ item.Product_ids }}</b-card-text>
+                        </b-card>
+                    </div>
                 </ul>
             </div>
         </div>
@@ -42,7 +43,7 @@
 </template>
 
 <script>
-    import axios from 'axios';
+    import Chemicals from "../public/chemical_score_details"
 
     export default {
         name: 'app',
@@ -55,7 +56,7 @@
                 selected: [],
                 modes: ['multi', 'single', 'range'],
                 selectMode: 'single',
-                chemicals: null,
+                chemicals: Chemicals,
                 chemical: null,
                 details: null,
                 fields: [
@@ -65,11 +66,6 @@
                 ],
             }
         },
-        mounted() {
-            axios
-                .get('http://localhost:3000/chemicals')
-                .then(response => (this.chemicals = response));
-        },
         methods: {
             onRowSelected(chemical) {
                 this.selected = chemical;
@@ -78,4 +74,7 @@
     }
 </script>
 
+<style>
+    b-card:
+</style>
 
